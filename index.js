3c7816609abcdf9901c66a2536d9df36d576bbe2
Marc Farra <marcfarra@gmail.com>
2015-09-01 18:12:37 -0400

    Add pipeline code for optimizing Promises. Fix _read bug

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var MetaStream = require('./lib/streams/MetaStream.js');
 var DataStream = require('./lib/streams/DataStream.js');
 var split = require('split');
 var Redis = require('ioredis');
-var Promise = require('bluebird');
 var util = require('util');
 var Readable = require('stream').Readable;
 
@@ -16,47 +15,91 @@ function PlanetStream (opts) {
 }
 
 PlanetStream.prototype._read = function () {
-  if (!this.started) this.run();
+  if (!this.started) {
+    this.run();
+    this.started = true;
+  }
 };
 
 PlanetStream.prototype.run = function () {
   var redis = new Redis();
   var stream = this;
 
+  var dataPipeline = redis.pipeline();
+  var metaPipeline = redis.pipeline();
+
+  var metaTimer, dataTimer = null;
+
   MetaStream().pipe(split(JSON.parse)).on('data', function (data) {
-    redis.set(data.id, JSON.stringify(data));
-    redis.expire(data.id, 600);
+    // Send data to redis after debounce
+    clearTimeout(metaTimer);
+    metaTimer = setTimeout(function () {
+      metaPipeline.exec(function () {});
+      metaPipeline = redis.pipeline();
+    }, 2000);
+
+    metaPipeline.set(data.id, JSON.stringify(data));
+    metaPipeline.expire(data.id, 600);
   });
 
   DataStream().pipe(split(JSON.parse)).on('data', function (data) {
-    redis.lpush('data:' + data.changeset, JSON.stringify(data));
-    redis.sadd('nometa', data.changeset);
-    redis.expire('data:' + data.changeset, 600);
+    // Send data to redis after debounce
+    clearTimeout(dataTimer);
+    dataTimer = setTimeout(function () {
+      dataPipeline.exec(function () {});
+      dataPipeline = redis.pipeline();
+    }, 2000);
+
+    dataPipeline.lpush('data:' + data.changeset, JSON.stringify(data));
+    dataPipeline.sadd('nometa', data.changeset);
+    dataPipeline.expire('data:' + data.changeset, 600);
   });
 
   function delay () { setTimeout(next, 60000); }
 
+  function changesetIdExists (changesetIds) {
+    var pipeline = redis.pipeline();
+    changesetIds.forEach(function (id) {
+      pipeline.get(id);
+    });
+    return pipeline.exec().then(function (results) {
+      return results.map(function (resultTuple, index) {
+        resultTuple[0] = changesetIds[index];
+        return resultTuple;
+      });
+    });
+  }
+
+  function joinData (existResults) {
+    var pipeline = redis.pipeline();
+    existResults.forEach(function (resultTuple) {
+      var id = resultTuple[0];
+      var metadata = resultTuple[1];
+      if (metadata) {
+        pipeline.lrange('data:' + id, 0, -1);
+      } else {
+        pipeline.sadd('staging', id);
+      }
+    });
+    return pipeline.exec().then(function (loadResults) {
+      loadResults.forEach(function (resultTuple, index) {
+        var resultData = resultTuple[1];
+        if (resultData !== 0 && resultData !== 1) { // data is not a sadd
+          resultData.forEach(function (listItem) {
+            var osmElement = JSON.parse(listItem);
+            osmElement.metadata = JSON.parse(existResults[index][1]);
+            console.log(JSON.stringify(osmElement));
+            stream.push(JSON.stringify(osmElement));
+          });
+        }
+      });
+    });
+  }
+
   function next () {
     redis.smembers('nometa')
-    .then(function (changesetIds) {
-      return Promise.all(changesetIds.map(function (id) {
-        return redis.get(id).then(function (metadata) {
-          if (metadata) {
-            redis.lrange('data:' + id, 0, -1).then(function (elements) {
-              elements.forEach(function (element) {
-                var toPush = JSON.parse(element);
-                toPush.metadata = JSON.parse(metadata);
-                stream.push(JSON.stringify(toPush) + '\n');
-              });
-            }).catch(function (err) {
-              throw new Error('lrange', err);
-            });
-          } else {
-            return redis.sadd('staging', id);
-          }
-        });
-      }));
-    })
+    .then(changesetIdExists)
+    .then(joinData)
     .then(function () {
       return redis.exists('staging').then(function () {
         return redis.rename('staging', 'nometa');
