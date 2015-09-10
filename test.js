7b66bf73751ee865e8522004e3b66d90a84ab36f
Marc Farra <marcfarra@gmail.com>
2015-09-10 17:41:39 -0400

    Add comment

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var planetStream = require('./');
 
-Only newly created buildings
+// Only newly created buildings
 planetStream().map(JSON.parse).filter(function (data) {
   return data.type === 'way' && data.action === 'create' &&
     data.tags.building;
