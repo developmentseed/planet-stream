bdae14a765165e1f7e24edbb98032ec7ba9d8995
Matthew Hanson <matt.a.hanson@gmail.com>
2016-01-29 17:24:48 -0500

    basic osm-planet-stream binary

--- bin/osm-planet-stream.js
+++ bin/osm-planet-stream.js
@@ -1,5 +1,34 @@
 #!/usr/bin/env node
 
-// planet-stream binary
+// start planet-stream
+var planetstream = require('../');
 
-require('../');
+// parse comments into hashtag list
+function getHashtags (str) {
+  if (!str) return [];
+  var wordlist = str.split(' ');
+  var hashlist = [];
+  wordlist.forEach(function (word) {
+    if (word.startsWith('#') && !R.contains(word, hashlist)) {
+      word = word.trim();
+      word = word.replace(/,\s*$/, '');
+      hashlist.push(word);
+    }
+  });
+  return hashlist;
+}
+
+// filter data here if desired
+planetstream.map(JSON.parse)
+.filter(function (data) {
+  return true;
+})
+// print out record
+.onValue(function (obj) { 
+  var payload = JSON.stringify(obj)
+  if (obj.metadata) {
+  	console.log(payload);
+  } else {
+    console.log('No metadata for ' + obj);
+  }
+});
