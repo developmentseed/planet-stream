fd95b9f805d3f726527e88745310a7926c7ebc9b
Matthew Hanson <matt.a.hanson@gmail.com>
2016-01-29 15:24:32 -0500

    cleanup and comments

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ var planetStream = require('./lib/planetstream.js');
 var kinesis = require('./lib/kinesis.js');
 var R = require('ramda');
 
-streamname = 'osmsim'
+var tracked = ['#missingmaps'];
 
+
+// start planet-stream
 var diffs = planetStream();
 
+// parse comments into hashtag list
 function getHashtags (str) {
   if (!str) return [];
   var wordlist = str.split(' ');
@@ -25,8 +28,7 @@ function getHashtags (str) {
   return hashlist;
 }
 
-var tracked = ['#osmgeoweek', '#missingmaps', '#hotosm-afghanistan-eq-1264', '#PeaceCorps'];
-
+// filter data for hashtags
 diffs.map(JSON.parse)
 .filter(function (data) {
   if (!data.metadata || !data.metadata.comment) {
@@ -37,6 +39,7 @@ diffs.map(JSON.parse)
 //  return intersection.length > 0;
   return hashtags.length > 0;
 })
+// add a complete record to kinesis
 .onValue(function (obj) { 
   var data = JSON.stringify(obj)
   if (obj.metadata) {
@@ -46,7 +49,7 @@ diffs.map(JSON.parse)
       StreamName: process.env.STREAM_NAME
     };
     kinesis.kin.putRecord(dataParams, function (err, data) {
-      if (err) console.err(err);
+      if (err) console.error(err);
       else console.log(data);
     });
   } else {
